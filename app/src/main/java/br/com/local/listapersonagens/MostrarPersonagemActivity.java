46b7888526be431231f9fe93668507a7348945d9
Augusto V. B. Oliveira <augusto.vinicius137@gmail.com>
2021-05-15 21:56:14 -0300

    MostrarPersonagem: Código que irá mostrar o personagem

---
+++ app/src/main/java/br/com/local/listapersonagens/MostrarPersonagemActivity.java
@@ -0,0 +1,14 @@
+package br.com.local.listapersonagens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MostrarPersonagemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mostrar_personagem_layout);
+    }
+}
