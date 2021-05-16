290a575682ca5f3ba6572b8712aef1c77dedb049
Augusto V. B. Oliveira <augusto.vinicius137@gmail.com>
2021-05-16 17:51:53 -0300

    MostrarPersonagem: Adicionado algoritmo para receber e definir valores de imagem e texto

--- app/src/main/java/br/com/local/listapersonagens/MostrarPersonagemActivity.java
+++ app/src/main/java/br/com/local/listapersonagens/MostrarPersonagemActivity.java
@@ -2,13 +2,31 @@ package br.com.local.listapersonagens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MostrarPersonagemActivity extends AppCompatActivity {
+    TextView txtPersonagens;
+    ImageView imgPersonagens;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mostrar_personagem_layout);
+
+        txtPersonagens = findViewById(R.id.mtxtPersonagem);
+        imgPersonagens = findViewById(R.id.mimgPersonagem);
+
+        Intent intent = getIntent();
+
+        String nPersonagem = intent.getStringExtra("nomePersonagens");
+        int mPersonagem = intent.getIntExtra("imgPersonagens", 0);
+
+        txtPersonagens.setText(nPersonagem);
+        imgPersonagens.setImageResource(mPersonagem);
     }
+
+
 }
