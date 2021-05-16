dbb8e1398ac65d8730d6b2d69809113d5ffb3814
Augusto V. B. Oliveira <augusto.vinicius137@gmail.com>
2021-05-15 21:54:30 -0300

    ListaPersonagensApp: Commit Inicial

---
+++ app/src/main/java/br/com/local/listapersonagens/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.local.listapersonagens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
